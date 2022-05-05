a16389e553a4a1d6ccf83e80d018ec47a10a5ebb
tallboy <yourspaceworkteam@gmail.com>
2022-05-05 20:33:02 +0300

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import Counter from "./components/Counter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Js1', body: 'Description'},
+        {id: 2, title: 'Js2', body: 'Description'},
+        {id: 3, title: 'Js3', body: 'Description'}
+    ]);
+
+    const [post, setPost] = useState({title: '', body: ''})
+
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        setPosts([...posts, {...post, id: Date.now() }])
+        setPost({title: '', body: ''})
+    }
+
+    return (
+        <div className="App">
+            <form>
+
+                <MyInput
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                    type="text"
+                    placeholder='Название'
+                />
+
+                <MyInput
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    type="text"
+                    placeholder='Описание'
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title={'Посты про JS 1'}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PostItem from "./PostItem";
+
+const PostList = ({posts, title}) => {
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}>
+                {title}
+            </h1>
+
+            {posts.map((post, index) =>
+                <PostItem
+                    post={post}
+                    key={post.id}
+                    number={index + 1}
+                />
+
+            )}
+        </div>
+    );
+};
+
+export default PostList;
