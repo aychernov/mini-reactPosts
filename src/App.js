c1902abac110122aaed38142cec50f7da65a94e2
tallboy <yourspaceworkteam@gmail.com>
2022-05-13 17:36:04 +0300

    add search, refactor components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, {useMemo, useRef, useState} from "react";
 import Counter from "./components/Counter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
@@ -7,6 +7,7 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -15,8 +16,19 @@ function App() {
         {id: 3, title: 'ааа', body: 'уу'}
     ]);
 
-    const [selectedSort, setSelectedSort] = useState('')
+    const [filter, setFilter] = useState({sort: '', query: ''})
 
+
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts;
+    }, [filter.sort, posts])
+
+    const sortedAndSearchPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    }, [filter.query, sortedPosts]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -26,33 +38,12 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    const sortPosts = (sort) =>{
-        setSelectedSort(sort)
-        console.log(sort)
-        setPosts( [...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
-    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <div>
-                <hr style={{margin: '15px 0px'}}/>
-                <MySelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue='Сортировка по'
-                    option={[
-                        {value: 'title', name: 'По названию'},
-                        {value: 'body', name: 'По описанию'},
-                    ]}
-
-                />
-            </div>
-            {posts.length
-                ? <PostList remove={removePost} posts={posts} title={'Посты про JS 1'}/>
-                : <h1 style={{textAlign: 'center'}}>Постов нет!</h1>
-            }
-
+            <PostFilter filter={filter} setFilter={setFilter}/>
+            <PostList remove={removePost} posts={sortedAndSearchPosts} title={'Посты про JS 1'}/>
         </div>
     );
 }
