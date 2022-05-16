0c274c9fb3d9cb34f1e17bcaabad393c79c4d7fb
tallboy <yourspaceworkteam@gmail.com>
2022-05-16 14:39:18 +0300

    create custom usePosts hook

--- src/App.js
+++ src/App.js
@@ -9,32 +9,20 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./hooks/usePost";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'яяя', body: 'вв'},
-        {id: 2, title: 'ввв', body: 'ее'},
-        {id: 3, title: 'ааа', body: 'уу'}
-    ]);
+    const [posts, setPosts] = useState([]);
 
     const [filter, setFilter] = useState({sort: '', query: ''})
-
     //Modal
     const [modal, setModal] = useState(false)
 
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
 
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts])
 
-    const sortedAndSearchPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts]);
-
-    const createPost = (newPost) => {
+    const
+    createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
@@ -46,11 +34,11 @@ function App() {
 
     return (
         <div className="App">
-            <MyButton style={{marginTop: '30px' }}
-                onClick={() => setModal(true)}>
+            <MyButton style={{marginTop: '30px'}}
+                      onClick={() => setModal(true)}>
                 Создать пост
             </MyButton>
-            <MyModal visible={modal} setVisible={setModal} >
+            <MyModal visible={modal} setVisible={setModal}>
                 <PostForm create={createPost}/>
             </MyModal>
 
