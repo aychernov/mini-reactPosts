f8ec96ecc53cf23dd8a97868c258168e0c998be5
tallboy <yourspaceworkteam@gmail.com>
2022-05-16 15:05:37 +0300

    add preLoader

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import {usePosts} from "./hooks/usePost";
 import axios from "axios";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -19,24 +20,25 @@ function App() {
     //Modal
     const [modal, setModal] = useState(false)
     const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostLoading, setIsPostLoading] = useState(false)
 
 
-    useEffect(() =>{
+    useEffect(() => {
         fetchPosts()
     }, [])
 
 
-
-
     const
-    createPost = (newPost) => {
-        setPosts([...posts, newPost])
-        setModal(false)
-    }
+        createPost = (newPost) => {
+            setPosts([...posts, newPost])
+            setModal(false)
+        }
 
-    async function fetchPosts(){
+    async function fetchPosts() {
+        setIsPostLoading(true)
         const posts = await PostService.getAll()
         setPosts(posts)
+        setIsPostLoading(false)
     }
 
 
@@ -56,7 +58,10 @@ function App() {
             </MyModal>
 
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList remove={removePost} posts={sortedAndSearchPosts} title={'Посты про JS 1'}/>
+            {isPostLoading
+                ? <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}} ><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchPosts} title={'Посты про JS 1'}/>
+            }
         </div>
     );
 }
